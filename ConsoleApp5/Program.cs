38a8a586b05f4114096deb60a97eab1e685e7046
huca06001 <huca06001@E-PW05C37R>
2024-11-07 12:15:24 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ändra bakgrundsfärg till blå
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            // Ändra textfärgen till vit
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Denna dator har fått ett fel");
+        }
+    }
+}
